c4d3fb2b031598dde22136307218b7d1b97dc2b9
Nancy Chen <nancy.sychen@hotmail.com>
2024-08-12 09:45:41 -0300

    create project with typescript

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	>
+
+	<main>Hello</main>
+</template>
+
+<script setup lang="ts"></script>
+
+
+<style scoped></style>
---
+++ src/components/TabLink.vue
@@ -0,0 +1,22 @@
+<template>
+    Link
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    currentTab: string;
+    tab: {
+        name: string;
+        label: string;
+    }
+}>();
+
+defineEmits<{
+    click: []
+}>()
+
+</script>
+
+<style scoped>
+
+</style>
